beb819baef5e315f0685243b598e2f9f0a8ddeb5
tyVespA <migliavacca.marco08@gmail.com>
2022-12-29 11:12:28 +0100

    Add applyColor function and prevent dragging behaviour on grid

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ const grid = document.querySelector(".grid");
 const clearGridBtn = document.querySelector(".clear-grid");
 const slider = document.querySelector("#grid-size");
 let gridSizeDisplay = document.querySelector(".grid-size-display");
-// let gridSize = document.querySelector("#grid-size").value;
+let cells = grid.childNodes;
 
+createGrid(20, 20);
+
+// prevents dragging when mousedown
+grid.addEventListener("dragstart", (e) => {
+  e.preventDefault();
+})
+grid.addEventListener('drop', (e) => {
+  e.preventDefault();
+})
+
+// grabs grid size slider's value and calls createGrid passing it
 slider.addEventListener("input", () => {
   let sliderVal = slider.value;
   gridSizeDisplay.textContent = sliderVal + " x " + sliderVal;
@@ -17,10 +28,32 @@ function createGrid(rows, cols) {
   grid.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
-    cell.innerText = ("");
-    grid.appendChild(cell).className = "grid-item";
+    grid.appendChild(cell);
+    cell.classList.add("grid-item-border");
+    cell.setAttribute('draggable', false);
   };
+
+  cells = grid.childNodes;
 }
+
+// apply color
+let mouseDown = false
+grid.onmousedown = () => (mouseDown = true)
+grid.onmouseup = () => (mouseDown = false)
+
+cells.forEach((cell) => {
+  cell.addEventListener("mousedown", () => {
+    cell.style.cssText = ("background: black");
+  })
+  cell.addEventListener("mouseover", () => {
+    if (mouseDown == true) {
+      cell.style.cssText = ("background: black");
+    }
+  })
+})
+
+
+
 
 
 
